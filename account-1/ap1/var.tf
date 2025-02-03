76a4cf5727196719b88b5f923d085e96cdf16a37
Amir Kamali <amirali.kouhikamali@ge.com>
2025-02-03 12:37:50 -0800

    changed bucket name

--- account-1/ap1/var.tf
+++ account-1/ap1/var.tf
@@ -1,5 +1,5 @@
 variable "s3_bucket_name" {
   type        = string
   description = "AWS Account number of the target account."
-  default     = "acc1-bucket-287563783486"
+  default     = "acc1-287563783486"
 }
--- account-2/ap1/var.tf
+++ account-2/ap1/var.tf
@@ -1,5 +1,5 @@
 variable "s3_bucket_name" {
   type        = string
   description = "AWS Account number of the target account."
-  default     = "acc2-bucket-287563783486"
+  default     = "acc2-287563783486"
 }
